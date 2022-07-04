d719e94bf18705ed0c310eaa4a787bc61b4afb72
Trainy32 <cjstkdthsu98@naver.com>
2022-07-05 03:07:37 +0900

    fix: 홈화면 기본 레이아웃 #6

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 // 컴포넌트
 import EditBubble from "../elements/EditBubble";
@@ -12,32 +12,38 @@ import { IoHeartOutline, IoChatbubbleOutline } from "react-icons/io5";
 
 const PostCard = (props) => {
   const navigate = useNavigate();
-  const postData = props.data.boardMain[0]
-  const postId = props.data.postId
+  const postData = props.data.boardMain[0];
+  const postId = props.data.postId;
 
-  const [bubbleOn, setBubbleOn] = React.useState(false)
-  
+  const [bubbleOn, setBubbleOn] = React.useState(false);
+
   const menuOpen = () => {
-    setBubbleOn(!bubbleOn)
-  }
+    setBubbleOn(!bubbleOn);
+  };
 
   return (
     <CardWrap>
       <CardHeader>
         <UserInfo>
-          <div id="profileImg" img={ postData.user[0].img }/>
-          <span id="nickName"> { postData.user[0].nickname } </span>
+          <div id="profileImg" img={postData.user[0].img} />
+          <span id="nickName"> {postData.user[0].nickname} </span>
         </UserInfo>
-        <IoMdMore id='optionMenu' onClick={menuOpen}/>
-        { bubbleOn ? <EditBubble contentsId={postId} setBubbleOn={setBubbleOn} /> : null}
+        <IoMdMore id="optionMenu" onClick={menuOpen} />
+        {bubbleOn ? <EditBubble contentsId={postId} setBubbleOn={setBubbleOn} /> : null}
       </CardHeader>
 
-      <ImgPreview img={ postData.imgURL[0]?.imgUrl }/>
-      <TextPreview> { postData.content } </TextPreview>
+      <Contents onClick={() => navigate('/post/detail/' + postId)}>
+        <ImgPreview img={postData.imgURL[0]?.imgUrl} />
+        <TextPreview> {postData.content} </TextPreview>
+      </Contents>
 
       <Reactions>
-        <span><IoHeartOutline/> { postData.likeCnt } </span>
-        <span><IoChatbubbleOutline/> { postData.viewCnt } </span>
+        <span>
+          <IoHeartOutline /> {postData.likeCnt}{" "}
+        </span>
+        <span>
+          <IoChatbubbleOutline /> {postData.viewCnt}{" "}
+        </span>
       </Reactions>
     </CardWrap>
   );
@@ -79,10 +85,17 @@ const UserInfo = styled.span`
   }
 `;
 
+const Contents = styled.div`
+  cursor: pointer;
+`
+
 const ImgPreview = styled.div`
   width: 100%;
   height: 310px;
-  background: url(${(props) => props.img ? props.img : 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FopbGC%2FbtrF9ZNhpja%2FY026LUE8lwKcGmfqJiO3SK%2Fimg.png'});
+  background: url(${(props) =>
+    props.img
+      ? props.img
+      : "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FopbGC%2FbtrF9ZNhpja%2FY026LUE8lwKcGmfqJiO3SK%2Fimg.png"});
   background-size: cover;
   background-position: center;
   margin: auto;
@@ -95,10 +108,10 @@ const TextPreview = styled.p`
 `;
 
 const Reactions = styled.div`
-  padding-top : 15px;
+  padding-top: 15px;
   border-top: 1px solid #eee;
 
   span {
     margin-right: 10px;
   }
-`
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ import NoticeSlide from "../components/NoticeSlide";
 
 // style
 import styled from "styled-components";
+import { GrNext } from "react-icons/gr";
 
 // redux
 import { useSelector, useDispatch } from "react-redux";
@@ -29,21 +30,83 @@ const Home = () => {
         <NoticeSlide />
       </div>
 
-      <div>
-        <p>오늘의 인기상</p>
-        <div />
-        <span>vs</span>
-        <div />
-      </div>
+      <RealTimeRanking>
+        <h3>오늘의 인기상(실시간 랭킹)</h3>
+        <RealTimeTopRanker>
+          <RankerPreview />
+            vs
+          <RankerPreview />
+        </RealTimeTopRanker>
+        <div className="votebar"> <span>000표</span> <span>000표</span> </div>
+      </RealTimeRanking>
 
-      <div>
+
+      <CategoryRanking>
+        <div className="rankingCate">
+          <span> 귀여움 </span>
+          <span> 멋짐 </span>
+          <span> 예쁨 </span>
+          <span> 웃김 </span>
+        </div>
+
+        <div className="cateRankerList">
+          <div className="cateRankerPic"/>
+          <div>
+            <p><span>1위</span> 닉네임입니다아</p>
+            <div className="votebar"><span>000표</span></div>
+          </div>
+        </div>
+
+        <div className="cateRankerList">
+          <div className="cateRankerPic"/>
+          <div>
+            <p><span>1위</span> 닉네임입니다아</p>
+            <div className="votebar"><span>000표</span></div>
+          </div>
+        </div>
+
+        <div className="cateRankerList">
+          <div className="cateRankerPic"/>
+          <div>
+            <p><span>1위</span> 닉네임입니다아</p>
+            <div className="votebar"><span>000표</span></div>
+          </div>
+        </div>
+
+      </CategoryRanking>
+
+      <SubTitle>
         <h3>산책 메이트 모집 글</h3>
-      </div>
+        <span>
+          <GrNext />
+        </span>
+      </SubTitle>
 
-      <div>
+      <FindMate>
+        <div className="cardimg" />
+        <div className="cardContents">
+          <h5>
+            {" "}
+            타이틀 위치 <span>3분 전</span>
+          </h5>
+          <p> 모집글 내용이 들어갑니다. 모집글 내용이 들어갑니다. 모집글 내용이 들어갑니다. </p>
+          <div className="cardFooter">
+            <div className="userProfile">
+              {" "}
+              <div /> 닉네임{" "}
+            </div>
+            <div className="participants"> 7/10 </div>
+          </div>
+        </div>
+      </FindMate>
+
+      <SubTitle>
         <h3>자랑하기</h3>
-      </div>
-      
+        <span>
+          <GrNext />
+        </span>
+      </SubTitle>
+
       {posts.map((post, i) => (
         <PostCard key={post.postId} data={post} />
       ))}
@@ -60,10 +123,118 @@ const Logo = styled.div`
     no-repeat center;
   background-size: 20%;
 `;
-const Banner = styled.div`
-  background: url("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOraWH%2FbtrGaJclSXN%2FGrJANxFBQGKNzizjQdX6uk%2Fimg.png")
-    no-repeat center;
-  background-size: contain;
-  height: 240px;
-  width: 100%;
+
+const SubTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 30px;
+  border: 1px solid #efefef;
+
+  h3 {
+    font-weight: bold;
+  }
+`;
+
+const RealTimeRanking = styled.div`
+display:flex;
+flex-direction: column;
+padding: 30px;
+
+h3 {
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  .votebar {
+    height:30px;
+    background: #eee;
+    margin: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    padding: 0px 20px;
+  }
+`;
+
+const RealTimeTopRanker = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const RankerPreview = styled.div`
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  background: #ddd;
+  border-radius: 200px;
+`;
+
+
+const CategoryRanking = styled.div`
+margin: 20px;
+.rankingCate {
+  display: flex;
+  justify-content: space-between;
+  padding: 30px;
+}
+
+.cateRankerList {
+  display:flex;
+  align-items: center;
+
+  border-bottom: 2px solid #ddd;
+}
+
+.cateRankerPic {
+  width: 80px;
+  height: 80px;
+  margin: 10px;
+  background: #ddd;
+  border-radius: 80px;
+}
+
+.votebar {
+    height:30px;
+    background: #eee;
+    margin: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    padding: 0px 20px;
+  }
+
+`
+
+const FindMate = styled.div`
+  width: 90%;
+  height: 150px;
+  margin: 20px auto;
+  padding: 10px;
+  border: 1px solid #eee;
+  padding: 10px;
+  display: flex;
+
+  .cardimg {
+    min-width: 130px;
+    height: 130px;
+    margin: 10px;
+    background: #ddd;
+    border-radius: 150px;
+  }
+
+  .cardContents {
+    margin: 20px;
+  }
+
+  h5 {
+    display: flex;
+    justify-content: space-between;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .cardFooter {
+    display: flex;
+    margin-top: 10px;
+  }
 `;
