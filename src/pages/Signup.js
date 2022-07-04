d2fe3a638274eae3028a94e2723b3605c39a2645
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-04 17:52:22 +0900

    feat: 회원가입 기능 추가 #3

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,23 +10,47 @@ import Step from '../components/Step';
 // react
 import { useState } from 'react';
 
+// router
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+// axios
+import axios from 'axios';
+import instance from '../shared/axios';
+
 const Signup = () => {
+  const navigate = useNavigate();
   const [step, setStep] = useState(0);
 
-  // 각 스텝마다 유저정보 저장할 state필요
+  const handleCoutValue = num => {
+    setStep(prev => prev + num);
+  };
 
-  const handleCoutValue = () => {
-    setStep(prev => prev + 1);
+  const handleSignup = async userData => {
+    try {
+      // const response = await instance.post('user/signup', { ...userData });
+      const response = await axios.post('http://localhost:5000/user', { ...userData });
+      console.log(response.data);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      navigate('/signup');
+    }
   };
 
   return (
     <Wrap>
-      <div>
-        <IoIosArrowBack size={30} style={{ padding: '83px 0 90px 30px', cursor: 'pointer' }} />
-      </div>
-      <Step step={step} onCountChange={handleCoutValue} />
+      <SignupForm>
+        <IoIosArrowBack
+          size={30}
+          style={{ padding: '83px 0 100px 30px', cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        />
+        <Step step={step} onCountChange={handleCoutValue} onSignup={handleSignup} />
+      </SignupForm>
     </Wrap>
   );
 };
 
+const SignupForm = styled.div``;
 export default Signup;
