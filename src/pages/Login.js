678dfc116a17c991945aae82a5f37fadb3146d24
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-09 11:33:25 +0900

    feat: 로그인 시 쿠키 값 저장 #2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,8 +13,18 @@ import { useNavigate } from 'react-router-dom';
 // axios
 import instance from '../shared/axios';
 
+// redux
+import { useDispatch } from 'react-redux';
+
+// storage
+import { setUserDB } from '../redux/modules/userSlice';
+
+// cookie
+import { setCookie } from '../shared/cookie';
+
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const eamilValue = useRef('');
   const pwdValue = useRef('');
@@ -22,12 +32,13 @@ const Login = () => {
   const login = async userInfo => {
     try {
       const response = await instance.post('/user/login', userInfo);
-      alert(response.data);
-      // 토큰값 저장하는 함수 필요
-      // 토큰값으로 유저정보 받아오기 api필요
+      setCookie('accessToken', response.data.data.token.accessToken);
+      setCookie('refreshToken', response.data.data.token.refreshToken);
+      alert(response.data.msg);
+      dispatch(setUserDB);
       navigate('/');
     } catch (error) {
-      alert(error);
+      window.alert(error.response.data.errorMessage);
       navigate('/login');
     }
   };
