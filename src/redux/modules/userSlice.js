bb6868df704abe1052a2b58e0ed281f9f55781d9
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-09 11:36:39 +0900

    add: 로그인 시 state값 넣기 테스트 #2

--- src/redux/modules/userSlice.js
+++ src/redux/modules/userSlice.js
@@ -1,23 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import instance from '../../shared/axios';
 
-// 예시
-export const loadUserDB = userData => {
-  return async function (dispatch) {
-    try {
-      dispatch(loadUser(userData));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-};
-
-export const getUserDB = () => {
+export const setUserDB = () => {
   return async function (dispatch) {
     try {
       // 토큰 값으로 해당 유저 정보 받아오기
-      // instance.get()
-      // dispatch(getUser(userData));
+      console.log('로그인');
+      const response = await instance.get('/user/userInfo');
+      console.log(response.data);
+      // dispatch(setUser(response));
     } catch (error) {
       console.log(error);
     }
@@ -28,13 +19,14 @@ const userSlice = createSlice({
   name: 'user',
   initialState: {
     list: [],
+    isLogin: false,
   },
   reducers: {
     // 예시
     // changeName: (state, action) => {
     //   state.name = action.payload;
     // },
-    getUser: (state, action) => {
+    setUser: (state, action) => {
       console.log(action.payload);
       state.list = action.payload;
     },
@@ -46,5 +38,5 @@ const userSlice = createSlice({
 });
 
 export const userActions = userSlice.actions;
-export const { getUser, loadUser } = userSlice.actions;
+export const { setUser, loadUser } = userSlice.actions;
 export default userSlice.reducer;
