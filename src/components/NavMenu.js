7ba40899c73dfe4418f711e6f0513b6fcd67c737
Trainy32 <cjstkdthsu98@naver.com>
2022-07-14 08:28:17 +0900

    fix: 하단메뉴바 CSS 수정 #17

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -18,10 +18,22 @@ const NavMenu = () => {
 
   return (
       <NavWrap>
-        <div onClick={()=> navigate('/post')}> 자랑하기 </div>
-        <button onClick={()=> setCircleOn(true)}> <BsPlusLg/> </button>
+        <Inner>
+        <LeftBtn onClick={()=> navigate('/post')}> 자랑하기 </LeftBtn>
+        <RightBtn onClick={()=> navigate('/')} > 릴스 </RightBtn>
+
+        <RoundOuter>
+        <MidCover/>
+          <RoundInner>
+          <RoundBtn  onClick={()=> setCircleOn(true)}>
+            <BsPlusLg/>
+          </RoundBtn>
+          </RoundInner>
+        </RoundOuter>
+        </Inner>
+
         { circleOn ? <NavCircle setCircleOn={setCircleOn}/> : null }
-        <div onClick={()=> navigate('/')}> 릴스 </div>
+
       </NavWrap>
   );
 };
@@ -29,42 +41,111 @@ const NavMenu = () => {
 export default NavMenu;
 
 const NavWrap = styled.div`
-  position: fixed;  
-  box-sizing: border-box;
+  position: fixed; 
   width: 100%;
   max-width: 600px;
-  height: 12.2vh;
+  height: 10.25vh;
   bottom: 0px;
   left: 50%;
   transform: translate(-50%, 0);
   z-index: 100;
+`
 
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
-  background: #fff;
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;  
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
+  div {
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+`;
+
+const LeftBtn = styled.div`
+    background: #fff;    
+    height:100%;
+    width: 45.3%;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.25);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #333;
+
+    border-radius: 0 3rem 0 0;
+    cursor: pointer;
+`
+
+const RightBtn = styled.div`
+    background: #fff;    
+    height:100%;
+    width: 45.3%;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.25);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #333;
+
+    border-radius: 3rem 0 0 0;
+    cursor: pointer;
+`
+
+const RoundOuter = styled.div`
+position: absolute;
+background: #fff;
+width: 16%;
+min-width: 6rem;
+max-width: 8rem;
+height: 16vw;
+min-height: 6rem;
+max-height: 8rem;
+border-radius: 6rem;
+box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.25);
+top: 0;
+left: 50%;
+transform: translate(-50%, 0);
+`
+const MidCover = styled.div`
+background: #fff;
+position: absolute;
+height:130%;
+width:110%;
+bottom: -50%;
+left:-5%;
+z-index: 100;
+`
+
+const RoundInner = styled.div`
+position: absolute;
+border-radius: 6rem;
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+box-shadow: 0px 4px 2px 0px rgba(0, 0, 0, 0.25);
+z-index: 200;
+
+cursor: pointer;
+`
+
+const RoundBtn = styled.button`
+  width: 73.3%;
+  height: 73.3%;
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 3%;
-
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 37%;
-    height: 8.5vh;
-    background: #ededed;
-    cursor: pointer;
-  }
-
-  button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 8.5vh;
-    height: 8.5vh;
-    min-height: 61px;
-    border-radius: 600px;
-  }
-`;
+  border-radius: 100px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.25);
+  background-color: #4addd0;
+  background-image: linear-gradient(to bottom, rgba(0, 148, 255, 0) 0%, #4ac6d0 100%);
+  color: #fff;
+`
