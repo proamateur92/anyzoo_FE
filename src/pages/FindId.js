f42057f9c3dd31e5ac2b719d52d352e502e7946c
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-19 22:37:41 +0900

    style: 이메일 찾기, 비밀번호 변경 테스트 #34

--- src/pages/FindId.js
+++ src/pages/FindId.js
@@ -10,7 +10,14 @@ import Wrap from '../elements/Wrap';
 // axios
 import instance from '../shared/axios';
 
+// icon
+import { IoIosArrowBack } from 'react-icons/io';
+
+// router
+import { useNavigate } from 'react-router-dom';
+
 const FindId = () => {
+  const navigate = useNavigate();
   const [enteredPhoneNumber, setEnteredPhoneNumber] = useState('');
 
   const handleSetPhoneNumber = (event) => {
@@ -18,38 +25,97 @@ const FindId = () => {
   };
 
   const handleFindId = async (event) => {
-    console.log('핸드폰 번호 찾기');
+    console.log('이메일 계정 찾기');
     event.preventDefault();
     const phoneNumber = enteredPhoneNumber;
+    console.log(phoneNumber);
 
     try {
-      const response = await instance.get('/user/find/lostEmail', { phoneNumber });
+      const response = await instance.get(`/user/find/lostEmail/${phoneNumber}`);
       console.log(response.data);
     } catch (error) {
       console.log(error);
     }
-
     setEnteredPhoneNumber('');
   };
 
   return (
     <Wrap>
       <FindForm>
-        <span>아이디찾기</span>
-        <input
-          value={enteredPhoneNumber}
-          onChange={handleSetPhoneNumber}
-          type='text'
-          placeholder="'-'없이 입력해주세요."
-        />
-        <button onClick={handleFindId}>계정 정보 확인</button>
+        <Top>
+          <IoIosArrowBack
+            style={{ position: 'absolute', cursor: 'pointer', left: '2%', fontSize: '25px' }}
+            onClick={() => navigate('/login')}
+          />
+          <span>이메일 찾기</span>
+        </Top>
+        <Container>
+          <Text>
+            <span>이메일 계정을 잊으셨나요?</span>
+            <span>ANYZOO 가입 시 입력한 휴대폰 번호를 입력해주세요.</span>
+          </Text>
+          <InputBox>
+            <span>휴대폰 번호</span>
+            <input
+              value={enteredPhoneNumber}
+              onChange={handleSetPhoneNumber}
+              type='text'
+              placeholder="'-'없이 입력해주세요."
+            />
+          </InputBox>
+          <button onClick={handleFindId}>계정 정보 확인</button>
+        </Container>
       </FindForm>
     </Wrap>
   );
 };
 
 const FindForm = styled.form`
+  height: 80vh;
+  background-color: blue;
+`;
+
+const Top = styled.div`
+  position: relative;
+  align-items: center;
+  padding: 4.5% 0 11.6% 0;
+  font-size: 20px;
+  font-weight: 800;
+  text-align: center;
+`;
+
+const Container = styled.div`
   display: flex;
   flex-direction: column;
+  height: 70%;
+  padding: 0 17.9%;
+  background-color: red;
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  span:first-of-type {
+    display: block;
+    font-size: 20px;
+    font-weight: 800;
+    margin-bottom: 2%;
+  }
+  span:nth-of-type(2) {
+    font-size: 12px;
+    margin-bottom: 10%;
+  }
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  span {
+    font-size: 12px;
+  }
+  input {
+    height: 17.9%;
+  }
 `;
 export default FindId;
--- src/pages/FindPassword.js
+++ src/pages/FindPassword.js
@@ -1,11 +1,88 @@
+// react
+import { useState } from 'react';
+
+// style
+import styled from 'styled-components';
+
+// elements
 import Wrap from '../elements/Wrap';
 
+// axios
+import instance from '../shared/axios';
+
+// icon
+import { IoIosArrowBack } from 'react-icons/io';
+
+// router
+import { useNavigate } from 'react-router-dom';
+
 const FindPassword = () => {
+  const navigate = useNavigate();
+  const [enteredPhoneNumber, setEnteredPhoneNumber] = useState('');
+
+  const handleSetPhoneNumber = (event) => {
+    setEnteredPhoneNumber(event.target.value);
+  };
+
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const handleSetPassword = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const handleNewPassword = async () => {
+    console.log('새 비밀번호 설정');
+
+    console.log('핸드폰번호');
+    console.log(enteredPhoneNumber);
+
+    console.log('비밀번호');
+    console.log(enteredPassword);
+
+    try {
+      const response = await instance.post('/user/make/newPassword', {
+        phoneNumber: enteredPhoneNumber,
+        newPassword: enteredPassword,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Wrap>
-      <span>비밀번호찾기</span>
+      <FindForm>
+        <Top>
+          <IoIosArrowBack style={{ cursor: 'pointer' }} onClick={() => navigate('/login')} />
+          <span>비밀번호 재설정</span>
+        </Top>
+        <div>
+          <span>비밀번호를 잊어도 걱정하지 마세요</span>
+        </div>
+        <input
+          value={enteredPhoneNumber}
+          onChange={handleSetPhoneNumber}
+          type='text'
+          placeholder="'-'없이 입력해주세요."
+        />
+        <input
+          value={enteredPassword}
+          onChange={handleSetPassword}
+          type='password'
+          placeholder='비밀번호를 입력하세요.'
+        />
+        {/* <input type='password' placeholder='' /> */}
+        <button onClick={handleNewPassword}>인증완료</button>
+      </FindForm>
     </Wrap>
   );
 };
+
+const Top = styled.div``;
+
+const FindForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default FindPassword;
