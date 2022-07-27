950f801c3cc873fb78a4adafc0219f8bc2b893a4
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-27 18:41:53 +0900

    feat: 소셜로그인 기능 구현 (화면 설계 필요, 가입 이슈로 기능 막아놓음) #35

--- src/components/Social/Google.js
+++ src/components/Social/Google.js
@@ -4,7 +4,7 @@ import logo from '../../assets/images/google.png';
 
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
-const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email&userinfo.profile`;
 
 const Google = () => {
   return (
--- src/pages/Oauth.js
+++ src/pages/Oauth.js
@@ -1,37 +1,81 @@
+// react
+import { useCallback, useEffect } from 'react';
+
+// redux
+import { useDispatch } from 'react-redux';
+
 // router
-import { useEffect } from "react";
-import { useLocation  } from "react-router-dom";
-import instance from "../shared/axios";
-// import axios from "axios";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+// userSlice
+import { setUserDB } from '../redux/modules/userSlice';
+
+// axios
+import instance, { setAccessToken } from '../shared/axios';
+
+// cookie
+import { setCookie } from '../shared/cookie';
+
+// sweetalert
+import Swal from 'sweetalert2';
 
 const Oauth = () => {
-	const location = useLocation();
-		const queryString = location.search;
-		console.log(queryString);
-		const codeStart = queryString.indexOf('=');
-		const codeEnd = queryString.indexOf('&');
-		const code = location.search.substring(codeStart+1, codeEnd);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [serachParams, setSearchParams] = useSearchParams();
+  const code = serachParams.get('code');
+  const scope = serachParams.get('scope');
 
-		const sendAuthCode = async() => {
-			try {
-				console.log('인가코드 체크', code);
-				await instance.post('/user/socialLogin', {
-					headers: {
-						code
-					}
-				});
-			} catch (error) {
-				console.log(error);
-			}
-		}
+  const sendAuthCode = useCallback(
+    async (url) => {
+      console.log('인가코드 체크', code);
+      // try {
+      //   console.log('인가코드 체크', code);
+      //   const response = await instance.get(url, {
+      //     headers: {
+      //       code,
+      //     },
+      //   });
+      //   console.log(response);
+      //   const accessToken = response.data.accesstoken;
+      //   const refreshToken = response.data.refreshtoken;
+      //   setCookie('accessToken', accessToken);
+      //   setCookie('refreshToken', refreshToken);
+      //   navigate('/');
+      //   setAccessToken();
+      //   dispatch(setUserDB());
+      // } catch (error) {
+      //   console.log(error);
+      //   Swal.fire({
+      //     title: '로그인에 실패하였습니다',
+      //     icon: 'error',
+      //     confirmButtonText: '확인',
+      //     confirmButtonColor: '#44DCD3',
+      //   }).then(() => {
+      //     navigate('/login');
+      //   });
+      // }
+    },
+    [code]
+  );
 
-		useEffect(()=>{
-			sendAuthCode();
-		},[sendAuthCode]);
+  useEffect(() => {
+    let url = '';
 
-	return(<>
-		<span>소셜로그인 인증 페이지</span>
-	</>)
-}
+    if (scope) {
+      url = '/user/oauth/google';
+    } else {
+      url = '/user/oauth/kakao';
+    }
+
+    sendAuthCode(url);
+  }, [scope, sendAuthCode]);
+
+  return (
+    <>
+      <span>소셜로그인 인증 페이지</span>
+    </>
+  );
+};
 
 export default Oauth;
