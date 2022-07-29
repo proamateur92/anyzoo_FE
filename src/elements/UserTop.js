8d7472f7a44015bf8bbb07ac5f2de18e71ef577a
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-29 18:29:02 +0900

    style: 상단 바 사이즈 조정 (vw -> %)

--- src/elements/UserTop.js
+++ src/elements/UserTop.js
@@ -1,72 +1,72 @@
 // style
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // icon
-import { IoIosArrowBack } from 'react-icons/io';
-import { FaSignOutAlt } from 'react-icons/fa';
+import { IoIosArrowBack } from "react-icons/io";
+import { FaSignOutAlt } from "react-icons/fa";
 
 // sweetalert
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 // router
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 // CircularProgressbar
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 // 로그아웃
-import { clearCookie } from '../shared/cookie';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 const UserTop = ({ title, type, step, moveStep, showLogout, percentage }) => {
   const myInfo = useSelector((state) => state.user.info);
   const navigate = useNavigate();
-  let icon = '';
+  let icon = "";
 
   const logout = () => {
     Swal.fire({
-      title: '로그아웃 하시겠습니까?',
-      icon: 'warning',
+      title: "로그아웃 하시겠습니까?",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: '로그아웃',
-      cancelButtonText: '취소',
-      confirmButtonColor: '#44DCD3',
+      confirmButtonText: "로그아웃",
+      cancelButtonText: "취소",
+      confirmButtonColor: "#44DCD3",
       reverseButtons: true,
     }).then((result) => {
       if (result.isConfirmed) {
-        clearCookie('accessToken');
-        clearCookie('refreshToken');
-        navigate('/login');
+        document.cookie = "accessToken" + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;path=/;";
+        document.cookie = "refreshToken" + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;path=/;";
+        navigate("/login");
+        window.location.reload();
       }
     });
   };
 
-  if (type === 'mypage' && showLogout && step === 0) {
-    icon = <FaSignOutAlt style={{ fontSize: '3rem', color: 'red' }} onClick={() => logout()} />;
-  } else if (type === 'signup') {
+  if (type === "mypage" && showLogout && step === 0) {
+    icon = <FaSignOutAlt style={{ fontSize: "3rem", color: "red" }} onClick={() => logout()} />;
+  } else if (type === "signup") {
     icon = (
       <CircularProgressbar
         value={percentage}
         text={`${percentage}%`}
         strokeWidth={12}
-        styles={buildStyles({ textColor: '#44DCD3', textSize: '30px', pathColor: '#44DCD3' })}
+        styles={buildStyles({ textColor: "#44DCD3", textSize: "30px", pathColor: "#44DCD3" })}
       ></CircularProgressbar>
     );
   }
   return (
     <Top type={type}>
-      {(type === 'signup' || type === 'find') && (
-        <Icon onClick={() => navigate('/login')}>
+      {(type === "signup" || type === "find") && (
+        <Icon onClick={() => navigate("/login")}>
           <IoIosArrowBack />
         </Icon>
       )}
-      {type === 'mypage' && title === '마이페이지' && (
-        <Icon onClick={() => (step === 0 ? navigate('/') : moveStep(0))}>
+      {type === "mypage" && title === "마이페이지" && (
+        <Icon onClick={() => (step === 0 ? navigate("/") : moveStep(0))}>
           <IoIosArrowBack />
         </Icon>
       )}
-      {type === 'mypage' && title !== '마이페이지' && (
+      {type === "mypage" && title !== "마이페이지" && (
         <Icon onClick={() => (step === 0 ? navigate(`/mypage/${myInfo.nickname}`) : moveStep(0))}>
           <IoIosArrowBack />
         </Icon>
@@ -84,7 +84,7 @@ const Top = styled.div`
   position: relative;
   font-size: 2rem;
   text-align: center;
-  height: 18vw;
+  padding: 6.5% 0;
   span {
     font-weight: 800;
   }
@@ -92,8 +92,8 @@ const Top = styled.div`
 
 const Icon = styled.div`
   position: absolute;
-  width: ${(props) => props.type === 'signup' && '11%'};
-  font-weight: ${(props) => props.type === 'signup' && '800'};
+  width: ${(props) => props.type === "signup" && "11%"};
+  font-weight: ${(props) => props.type === "signup" && "800"};
   cursor: pointer;
   font-size: 3rem;
   &:first-of-type {
