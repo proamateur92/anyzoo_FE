b263263ad3945f745e8cdb6531549e4270d0b9c1
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-07-29 19:35:58 +0900

    fix: 소셜로그인 주석 해제 35#

--- src/pages/Oauth.js
+++ src/pages/Oauth.js
@@ -1,71 +1,69 @@
 // react
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect } from "react";
 
 // redux
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 // router
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 // userSlice
-import { setUserDB } from '../redux/modules/userSlice';
+import { setUserDB } from "../redux/modules/userSlice";
 
 // axios
-import instance, { setAccessToken } from '../shared/axios';
+import instance, { setAccessToken } from "../shared/axios";
 
 // cookie
-import { setCookie } from '../shared/cookie';
+import { setCookie } from "../shared/cookie";
 
 // sweetalert
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 const Oauth = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [serachParams, setSearchParams] = useSearchParams();
-  const code = serachParams.get('code');
-  const scope = serachParams.get('scope');
+  const code = serachParams.get("code");
+  const scope = serachParams.get("scope");
 
   const sendAuthCode = useCallback(
     async (url) => {
-      console.log('인가코드 체크', code);
-      // try {
-      //   console.log('인가코드 체크', code);
-      //   const response = await instance.get(url, {
-      //     headers: {
-      //       code,
-      //     },
-      //   });
-      //   console.log(response);
-      //   const accessToken = response.data.accesstoken;
-      //   const refreshToken = response.data.refreshtoken;
-      //   setCookie('accessToken', accessToken);
-      //   setCookie('refreshToken', refreshToken);
-      //   navigate('/');
-      //   setAccessToken();
-      //   dispatch(setUserDB());
-      // } catch (error) {
-      //   console.log(error);
-      //   Swal.fire({
-      //     title: '로그인에 실패하였습니다',
-      //     icon: 'error',
-      //     confirmButtonText: '확인',
-      //     confirmButtonColor: '#44DCD3',
-      //   }).then(() => {
-      //     navigate('/login');
-      //   });
-      // }
+      try {
+        const response = await instance.get(url, {
+          headers: {
+            code,
+          },
+        });
+        console.log(response);
+        const accessToken = response.data.accesstoken;
+        const refreshToken = response.data.refreshtoken;
+        setCookie("accessToken", accessToken);
+        setCookie("refreshToken", refreshToken);
+        navigate("/");
+        setAccessToken();
+        dispatch(setUserDB());
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: "로그인에 실패하였습니다",
+          icon: "error",
+          confirmButtonText: "확인",
+          confirmButtonColor: "#44DCD3",
+        }).then(() => {
+          navigate("/login");
+        });
+      }
     },
     [code]
   );
 
   useEffect(() => {
-    let url = '';
+    let url = "";
 
     if (scope) {
-      url = '/user/oauth/google';
+      url = "/user/oauth/google";
     } else {
-      url = '/user/oauth/kakao';
+      url = "/user/oauth/kakao";
     }
 
     sendAuthCode(url);
